a4eeba53f7f2a927f463c629aee76dd52203c3f5
Chinmoy Das <cdchinmoy@gmail.com>
2020-02-25 01:49:39 +0530

    initial changes

--- example_pkg/__init__.py
+++ example_pkg/__init__.py
@@ -4,7 +4,7 @@ import calendar
 
 class DateConvert:
 
-    def findDay(self, date): 
+    def find_day(self, date): 
         born = datetime.datetime.strptime(date, '%d-%m-%Y').weekday() 
         print(calendar.day_name[born]) 
         return calendar.day_name[born]
